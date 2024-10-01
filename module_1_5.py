5fa629d38d8bf81eca87cda2595714615175a300
ksaysv <ksaysv@mail.ru>
2024-10-01 23:07:05 +0300

    cat

--- module_1_5.py
+++ module_1_5.py
@@ -2,4 +2,4 @@ immutable_var=[3,4,'1s']
 print('Immutable tuple:', tuple(immutable_var))
 mutable_list=[1,2,3,'cat']
 mutable_list.append('Modified')
-print(mutable_list)
+print("Mutable list:", mutable_list)
