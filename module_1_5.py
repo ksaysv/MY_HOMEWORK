4c69c691ef29d35c3563dccf61baf11575319110
ksaysv <ksaysv@mail.ru>
2024-10-01 23:38:45 +0300

    cat

--- module_1_5.py
+++ module_1_5.py
@@ -1,5 +1,11 @@
-immutable_var=[3,4,'1s']
-print('Immutable tuple:', tuple(immutable_var))
-mutable_list=[1,2,3,'cat']
+immutable_var = [3,4, '1s']
+print("Дан список:", immutable_var)
+immutable_var.append('Modified')
+immutable_var=tuple([3,4,'1s'])
+#Теперь не возможно изменить список immutable_var, командой "immutable_var.append('Modified')", так как список зафиксирован функцией tuple
+print('Immutable tuple:', immutable_var)
+mutable_list=[3,4, '1s']
 mutable_list.append('Modified')
 print("Mutable list:", mutable_list)
+print("Вызванный второй элемент из списка Immutable var", immutable_var[1])
+print("Вызванный третий элемент из списка  Mutable list", mutable_list[2])
